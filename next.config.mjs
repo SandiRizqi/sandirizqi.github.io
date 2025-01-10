90ff0fb0be0ccead15a15737d15a45ce79b4b370
SANDI <sandi@SANDIs-MacBook-Pro.local>
2025-01-10 16:29:40 +0700

    build static

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Mengaktifkan static exports
+  basePath: "/static", // Ganti dengan nama repository Anda
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
